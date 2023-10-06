e69e32467066c6609d13a2a440b43a3c79600f74
Nikhil-prog <prog.nikhil@gmail.com>
2023-10-06 06:12:07 +0530

    added algofactory to return different types of algorithm as object

--- algorithms/algo_factory.py
+++ algorithms/algo_factory.py
@@ -1,11 +1,24 @@
+from .a_star import ASTAR
 from .bfs import BFS
+from .dfs import DFS
+from .dls import DLS
+from .greedy import GREEDY
+from .ids import IDS
+from .ucs import UCS
 
 class AlgoFactory() :
     @staticmethod
     def get_algo_object(type) :
-        algos = {"bfs" : BFS}
+        algos = {
+            "a_star" : ASTAR,
+            "bfs" : BFS,
+            "dfs" : DFS,
+            "dls"   : DLS,
+            "greedy" : GREEDY,
+            "ids" : IDS,
+            "ucs" : UCS,
+        }
         algo_names = algos.keys()
         
         assert type in algo_names, f"Pick algo from {', '.join(algo_names)}!"
-        
         return algos[type]
